9c3397ce941026a6003ea070536b79e6a1446d55
github-action-benchmark <github@users.noreply.github.com>
2025-07-08 14:34:41 +0000

    add Qwik CI Benchmarks (customSmallerIsBetter) benchmark result for 82b191755462b9e2ee3c06862283991e5838b83d

---
+++ dev/bench/data.js
@@ -0,0 +1,50 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1751985281645,
+  "repoUrl": "https://github.com/dibshrestha-kunai/qwik",
+  "entries": {
+    "Qwik CI Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "dibyamshushrestha@Dibyamshus-MacBook-Air.local",
+            "name": "Dibyamshu Shrestha"
+          },
+          "committer": {
+            "email": "dibyamshushrestha@Dibyamshus-MacBook-Air.local",
+            "name": "Dibyamshu Shrestha"
+          },
+          "distinct": true,
+          "id": "82b191755462b9e2ee3c06862283991e5838b83d",
+          "message": "Fix: pnpm setup in benchmark workflow 9",
+          "timestamp": "2025-07-08T09:31:07-05:00",
+          "tree_id": "4fb00a7e48be8d72df892d194f65a19b5134075a",
+          "url": "https://github.com/dibshrestha-kunai/qwik/commit/82b191755462b9e2ee3c06862283991e5838b83d"
+        },
+        "date": 1751985280816,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Build Time",
+            "value": "87.35",
+            "unit": "seconds"
+          },
+          {
+            "name": "Build Size",
+            "value": "7334.55",
+            "unit": "KB"
+          },
+          {
+            "name": "Runtime Execution Time",
+            "value": "0.95",
+            "unit": "ms"
+          },
+          {
+            "name": "Memory Usage",
+            "value": "4.33",
+            "unit": "MB"
+          }
+        ]
+      }
+    ]
+  }
+}
